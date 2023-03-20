f34d8cc5e795b21801ccb899b76d946a14f6a3c4
harshj-1703 <harshj6680@gmail.com>
2023-03-20 20:58:19 +0530

    nodemcu with php api home automation

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Login</title>
+    <link rel="icon" type="image" href="./img/home.jpg">
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+    <?php
+    require('db.php');
+    session_start();
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);
+        $username = mysqli_real_escape_string($con, $username);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $query = "SELECT * FROM `users` WHERE username='$username'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        // print_r($row['status']);
+        if ($rows == 1) {
+            $row = $result->fetch_assoc();
+            // echo $row['status'];
+            $_SESSION['username'] = $username;
+            if ($row['status'] == 1) {
+                header("Location: admin.php");
+            } else {
+                header("Location: index.php");
+            }
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+        ?>
+        <form class="form" method="post" name="login">
+            <h1 class="login-title">Login</h1>
+            <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true" />
+            <input type="password" class="login-input" name="password" placeholder="Password" />
+            <input type="submit" value="Login" name="submit" class="login-button" />
+            <p class="link">Don't have an account? <a href="registration.php">Registration Now</a></p>
+        </form>
+        <?php
+    }
+    ?>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,36 @@
+<?php
+include("auth_session.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home Automation</title>
+    <link rel="stylesheet" href="./css/navbar.css">
+    <link rel="icon" type="image" href="./img/home.jpg">
+</head>
+
+<body>
+    <nav class="navbar">
+        <h1 class="logo">Home Automation</h1>
+        <ul class="menu-items">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li class="dropdown">
+                <a href="#">Control</a>
+                <ul class="dropdown-menu">
+                    <li><a href="switch.php">Switch</a></li>
+                    <li><a href="meter.php">Meter</a></li>
+
+                </ul>
+            </li>
+            <li><a href="logout.php">Logout</a></li>
+            <li><a>Welcome!
+                    <?php echo $_SESSION['username']; ?>
+                </a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
